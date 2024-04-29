aa4fa65ad7e9151832a75cc407b58ccbd6d5b5c3
edumoreira7 <161092739+edumoreira7@users.noreply.github.com>
2024-04-29 23:21:27 +0000

    Criar funções e organizar estrutura

---
+++ banco.c
@@ -0,0 +1,38 @@
+#include "banco.h"
+#include <stdio.h>
+
+int adicionarC(Cliente clientes[], int *pos){
+  printf("funcao de criar contato\n");
+  return 0;
+}
+int deletarC(Cliente clientes[], int *pos){
+  printf("funcao de deletar contato\n");
+  return 0;
+}
+int listarC(Cliente clientes[], int pos){
+  printf("funcao de listar contato\n");
+  return 0;
+}
+int debito(Cliente clientes[], int pos){
+  printf("funcao de debito\n");
+  return 0;
+}
+int deposito(Cliente clientes[], int pos){
+  printf("funcao de deposito\n");
+  return 0;
+}
+int extrato(Cliente clientes[], int pos){
+  printf("funcao de extrato\n");
+  return 0;
+}
+int transferencia(Cliente clientes[], int pos){
+  printf("funcao de transferencia\n");
+  return 0;
+}
+
+int salvarC(Cliente clientes[], int total, int pos){
+  return 0;
+}
+int carregarC(Cliente clientes[], int total, int pos){
+  return 0;
+}
